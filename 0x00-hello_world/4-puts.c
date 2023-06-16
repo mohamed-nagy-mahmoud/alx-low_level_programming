e2f04925d7bcc75780e2898ab5726c2a4165a014
mohamed-nagy-mahmoud <mohamed.nagy.mahmoud@gmail.com>
2023-06-16 08:32:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main - a program that print an output line with puts function
+ * return: 0 (success)
+ */
+int main(void)
+{puts ("\"Programming is like building a multilingual puzzle");
+return(0);
+}
