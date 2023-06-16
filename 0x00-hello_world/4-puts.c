1f7a05ce68a8b3828d1bd3990dc74faaa981a9d3
mohamed-nagy-mahmoud <mohamed.nagy.mahmoud@gmail.com>
2023-06-16 08:58:55 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
  * main - a program that print an output line with puts function
- * return: 0 (success)
+ * return: 0(success)
  */
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle");
-return(0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This program uses printf to print the statement "...but the outcome is a piece of art,"followed by a new line. It then returns 0
+to indicate successful execution of the program.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
